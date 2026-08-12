2fbfb0a7b7bd4389841c7c6ebb8d92a174bf6541
Basavaraj M. Bastawadi <basavarajbastawadi7@gmail.com>
2026-08-12 17:47:44 +0530

    Refactor main function for vehicle details and rent
    Refactor main function to display details and calculate rent for car and bike separately.

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from car import Car
 from bike import Bike
 
-def main():
-    car = Car("KA01AB1234", "Toyota", 1500, 5)
-    bike = Bike("KA02CD5678", "Honda", 500, 125)
 
-    vehicles = [car, bike]
+car = Car("KA01AB1234", "Toyota", 1500, 5)
 
-    days = 3
+bike = Bike("KA02CD5678", "Honda", 500, 125)
 
-    for vehicle in vehicles:
-        vehicle.display_details()
 
-        rent = vehicle.calculate_rent(days)
+# Car details
+car.display_details()
 
-        print("Rental days:", days)
-        print("Total rent:", rent)
-        print("--------------------")
-main()
+days = 3
+rent = car.calculate_rent(days)
+
+print("Rental days:", days)
+print("Total rent:", rent)
+print("--------------------")
+
+
+# Bike details
+bike.display_details()
+
+days = 3
+rent = bike.calculate_rent(days)
+
+print("Rental days:", days)
+print("Total rent:", rent)
+print("--------------------")
